5382a069d3e97c1bcb3a6c29cf407512c69bc3c5
unknown <Administrador@HENRIQZIMER.LOCAL>
2023-12-19 23:15:42 -0300

    salvando

---
+++ StartPack.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo Instalando TeamViewer...
+echo y | winget install -e --id TeamViewer.TeamViewer
+
+echo Instalando Google Chrome...
+echo y | winget install -e --id Google.Chrome
+
+echo Instalando Microsoft Office...
+echo y | winget install -e --id Microsoft.Office
+
+echo Instalando WinRAR...
+echo y | winget install -e --id RARLab.WinRAR
+
+echo Instalando FortiClient VPN...
+echo y | winget install -e --id Fortinet.FortiClientVPN
+
+echo Instalação concluída.
